c8a892b465bae1a3b328da5b2650f72e33c4f569
Weiqi Ji <jiweiqi717@gmail.com>
2021-08-04 14:05:16 -0500

    assembling

--- Arduino/Servo/examples/Knob/Knob.ino
+++ Arduino/Servo/examples/Knob/Knob.ino
@@ -10,18 +10,22 @@
 #include <Servo.h>
 
 Servo myservo;  // create servo object to control a servo
+Servo arm3;
 
 int potpin = 0;  // analog pin used to connect the potentiometer
 int val;    // variable to read the value from the analog pin
 
 void setup() {
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+  arm3.attach(10);
 }
 
 void loop() {
   val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
-  val = map(val, 0, 1023, 0, 180);     // scale it to use it with the servo (value between 0 and 180)
-  myservo.write(val);                  // sets the servo position according to the scaled value
-  delay(15);                           // waits for the servo to get there
+  int pos1 = map(val, 100, 1000, 100, 180);     // scale it to use it with the servo (value between 0 and 180)
+  myservo.write(pos1);                  // sets the servo position according to the scaled value
+  delay(10);
+  int pos2 = map(val, 100, 1000, 0, 180);
+  arm3.write(pos2);
+  delay(20);                           // waits for the servo to get there
 }
-
